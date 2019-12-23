5bc6d911ddc8c293672fee99f1e0fd332b664ffd
zhenhua.zhang@alo7.com <zhenhua.zhang@alo7.com>
2019-12-23 08:32:07 +0800

    gateway doc

---
+++ spring-cloud-gateway/code/gateway-example1/src/test/java/com/example/gateway/gatewayexample/GatewayExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gateway.gatewayexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
